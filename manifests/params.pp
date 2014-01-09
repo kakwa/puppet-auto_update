59c57217eb0fe015271ca4cbb5faa24969578a64
pcarpent <pcarpent@psyche.si.c-s.fr>
2014-01-09 11:26:45 +0100

    adding logging of updated packages in RedHat

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,14 +6,14 @@ class auto_update::params {
         'Debian': {
             $cron_daily_dir = '/etc/cron.daily/'
             $update_command = 'aptitude clean; aptitude update; DEBIAN_FRONTEND=noninteractive aptitude upgrade -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"  -f -q -y'
-            $updated_packages_command = 'grep "^`date +%F`" /var/log/dpkg.log |grep upgrade|awk \'{print $4" (old:"$5") (new:"$6")"}\''
+            $updated_packages_command = 'grep "^`date +%F`" /var/log/dpkg.log |grep " upgrade \| install "|awk \'{print "package update: "$4" (old:"$5") (new:"$6")"}\''
         }
         
         #RedHat family variables
         'RedHat': {
             $cron_daily_dir = '/etc/cron.daily/'
             $update_command = 'yum clean all;yum update -y'
-            $updated_packages_command = 'printf ""'
+            $updated_packages_command = 'rpm -qa --last|grep "`date +\'%a %d %b %Y\'`"|awk \'{print "package update: "$1;}\''
         }
     }
 }
